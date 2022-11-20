b73dd7b9c38770c3ffc16a22c021c9689b28a181
glowwserrano7 <glorian.serrano1@upr.edu>
2022-11-20 13:05:31 -0400

    fractal methods fixed

--- src/fractal.hpp
+++ src/fractal.hpp
@@ -1,11 +1,13 @@
 #pragma once 
+#include <string>
 
+using namespace std; 
 class Fractal{
     public:
         virtual ~Fractal () {};
         
         virtual void draw () = 0;
-        virtual void getName () = 0;
-        virtual void getLevel ()= 0; 
+        virtual string getName () = 0;
+        virtual int getLevel ()= 0; 
 };
 
