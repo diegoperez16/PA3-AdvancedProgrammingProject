068bdb9ea03d61150909f2f356a452c98906c4a5
glowwserrano7 <glorian.serrano1@upr.edu>
2022-11-22 18:58:37 -0400

    fractal interface

--- src/fractal.hpp
+++ src/fractal.hpp
@@ -1,10 +1,11 @@
 #pragma once 
+
 #include <string>
 
 using namespace std; 
 class Fractal{
     public:
-        virtual ~Fractal () {};
+        // virtual ~Fractal () {};
         
         virtual void draw () = 0;
         virtual string getName () = 0;
